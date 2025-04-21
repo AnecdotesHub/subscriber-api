1bc1f530065c97f657fe9edd7283ba9aa5e8caf3
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-04-21 21:07:07 +0300

    Create CorsDefinition.cs

---
+++ src/Jevstafjev.Anecdotes.SubscriberApi/Jevstafjev.Anecdotes.SubscriberApi.Web/Definitions/Cors/CorsDefinition.cs
@@ -0,0 +1,40 @@
+﻿using Jevstafjev.Anecdotes.SubscriberApi.Domain;
+using Jevstafjev.Anecdotes.SubscriberApi.Web.Definitions.Base;
+
+namespace Jevstafjev.Anecdotes.SubscriberApi.Web.Definitions.Cors;
+
+public class CorsDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        var origins = builder.Configuration.GetSection("Cors").GetSection("Origins").Value?.Split(',');
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy(AppData.PolicyCorsName, policyBuilder =>
+            {
+                policyBuilder.AllowAnyHeader();
+                policyBuilder.AllowAnyMethod();
+
+                if (origins is not { Length: > 0 })
+                {
+                    return;
+                }
+
+                if (origins.Contains("*"))
+                {
+                    policyBuilder.AllowAnyHeader();
+                    policyBuilder.AllowAnyMethod();
+                    policyBuilder.SetIsOriginAllowed(host => true);
+                    policyBuilder.AllowCredentials();
+                }
+                else
+                {
+                    foreach (var origin in origins)
+                    {
+                        policyBuilder.WithOrigins(origin);
+                    }
+                }
+            });
+        });
+    }
+}

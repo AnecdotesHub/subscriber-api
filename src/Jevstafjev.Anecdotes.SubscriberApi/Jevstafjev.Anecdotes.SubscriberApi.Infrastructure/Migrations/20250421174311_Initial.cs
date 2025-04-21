aaa5ac73cd2f60a20cd9451804c6434bd4b89ae2
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-04-21 20:45:10 +0300

    Create migration

---
+++ src/Jevstafjev.Anecdotes.SubscriberApi/Jevstafjev.Anecdotes.SubscriberApi.Infrastructure/Migrations/20250421174311_Initial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jevstafjev.Anecdotes.SubscriberApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Subscribers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subscribers", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscribers_Email",
+                table: "Subscribers",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Subscribers");
+        }
+    }
+}

38a9bc059d454ae0ec4400fc53557c11d2ed0406
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-04-21 21:06:54 +0300

    Create CommonDefinition.cs

---
+++ src/Jevstafjev.Anecdotes.SubscriberApi/Jevstafjev.Anecdotes.SubscriberApi.Web/Definitions/Common/CommonDefinition.cs
@@ -0,0 +1,17 @@
+﻿using Jevstafjev.Anecdotes.SubscriberApi.Web.Definitions.Base;
+
+namespace Jevstafjev.Anecdotes.SubscriberApi.Web.Definitions.Common;
+
+public class CommonDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddRazorPages();
+    }
+
+    public override void ConfigureApplication(WebApplication app)
+    {
+        app.UseHttpsRedirection();
+        app.MapRazorPages();
+    }
+}

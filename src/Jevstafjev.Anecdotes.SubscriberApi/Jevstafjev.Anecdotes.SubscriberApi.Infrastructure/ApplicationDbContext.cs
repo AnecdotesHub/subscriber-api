696110aac2ad908950e7d36b444d0817ffcb5928
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-04-21 20:10:26 +0300

    Create ApplicationDbContext.cs

---
+++ src/Jevstafjev.Anecdotes.SubscriberApi/Jevstafjev.Anecdotes.SubscriberApi.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Jevstafjev.Anecdotes.SubscriberApi.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jevstafjev.Anecdotes.SubscriberApi.Infrastructure;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<Subscriber> Subscribers { get; set; }
+}

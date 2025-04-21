ae8032a70e4ff60051a243fbd88c768e3ec83806
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-04-21 21:12:54 +0300

    Create MediatorDefinition.cs

---
+++ src/Jevstafjev.Anecdotes.SubscriberApi/Jevstafjev.Anecdotes.SubscriberApi.Web/Definitions/Mediator/MediatorDefinition.cs
@@ -0,0 +1,14 @@
+﻿using Jevstafjev.Anecdotes.SubscriberApi.Web.Definitions.Base;
+using Jevstafjev.Anecdotes.SubscriberApi.Web.Definitions.FluentValidation;
+using MediatR;
+
+namespace Jevstafjev.Anecdotes.SubscriberApi.Web.Definitions.Mediator;
+
+public class MediatorDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidatorBehavior<,>));
+        builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblyContaining<Program>());
+    }
+}

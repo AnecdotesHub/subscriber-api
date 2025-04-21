07c885dcb7fc5df2c9799ec3173d19e6c531d778
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-04-21 20:42:02 +0300

    Create DbContextDefinition.cs

---
+++ src/Jevstafjev.Anecdotes.SubscriberApi/Jevstafjev.Anecdotes.SubscriberApi.Web/Definitions/DbContext/DbContextDefinition.cs
@@ -0,0 +1,17 @@
+﻿using Jevstafjev.Anecdotes.SubscriberApi.Infrastructure;
+using Jevstafjev.Anecdotes.SubscriberApi.Web.Definitions.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jevstafjev.Anecdotes.SubscriberApi.Web.Definitions.DbContext;
+
+public class DbContextDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            var connectionString = builder.Configuration.GetConnectionString(nameof(ApplicationDbContext));
+            options.UseSqlServer(connectionString);
+        });
+    }
+}

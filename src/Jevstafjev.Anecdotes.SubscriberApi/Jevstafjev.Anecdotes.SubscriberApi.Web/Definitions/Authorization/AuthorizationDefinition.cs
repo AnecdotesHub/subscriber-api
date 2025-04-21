85a4c0b3ea3bf4b3c24c0fe2910edb7688445042
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-04-21 21:07:01 +0300

    Create AuthorizationDefinition.cs

---
+++ src/Jevstafjev.Anecdotes.SubscriberApi/Jevstafjev.Anecdotes.SubscriberApi.Web/Definitions/Authorization/AuthorizationDefinition.cs
@@ -0,0 +1,84 @@
+﻿using Duende.IdentityModel;
+using Jevstafjev.Anecdotes.SubscriberApi.Domain;
+using Jevstafjev.Anecdotes.SubscriberApi.Web.Definitions.Base;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text.Json;
+
+namespace Jevstafjev.Anecdotes.SubscriberApi.Web.Definitions.Authorization;
+
+public class AuthorizationDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        var url = builder.Configuration.GetSection("AuthServer").GetValue<string>("Url");
+
+        builder.Services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, "Bearer", options =>
+                {
+                    options.SaveToken = true;
+                    options.Authority = url;
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = JwtClaimTypes.Name,
+                        ValidateAudience = true,
+                        ValidAudience = "AnecdoteApi"
+                    };
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnChallenge = context =>
+                        {
+                            context.HandleResponse();
+                            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                            context.Response.ContentType = "application/json";
+
+                            if (string.IsNullOrEmpty(context.Error))
+                            {
+                                context.Error = "invalid_token";
+                            }
+
+                            if (string.IsNullOrEmpty(context.ErrorDescription))
+                            {
+                                context.ErrorDescription = "This request requires a valid JWT access token to be provided";
+                            }
+
+                            if (context.AuthenticateFailure != null && context.AuthenticateFailure.GetType() == typeof(SecurityTokenExpiredException))
+                            {
+                                var authenticationException = context.AuthenticateFailure as SecurityTokenExpiredException;
+                                context.Response.Headers.Append("x-token-expired", authenticationException?.Expires.ToString("o"));
+                                context.ErrorDescription = $"The token expired on {authenticationException?.Expires:o}";
+                            }
+
+                            return context.Response.WriteAsync(JsonSerializer.Serialize(new
+                            {
+                                error = context.Error,
+                                error_description = context.ErrorDescription
+                            }));
+                        }
+                    };
+                });
+
+        builder.Services.AddAuthorization(options =>
+        {
+            options.AddPolicy(AppData.DefaultPolicyName, x =>
+            {
+                x.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme);
+                x.RequireAuthenticatedUser();
+            });
+        });
+    }
+
+    public override void ConfigureApplication(WebApplication app)
+    {
+        app.UseCors(AppData.PolicyCorsName);
+        app.UseAuthentication();
+        app.UseAuthorization();
+    }
+}

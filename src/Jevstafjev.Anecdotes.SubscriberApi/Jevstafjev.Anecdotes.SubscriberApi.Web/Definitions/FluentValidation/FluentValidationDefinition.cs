aeacf8e344420f52ecec38264008e7ba746a5bc4
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-04-21 21:08:37 +0300

    Add FluentValidation

---
+++ src/Jevstafjev.Anecdotes.SubscriberApi/Jevstafjev.Anecdotes.SubscriberApi.Web/Definitions/FluentValidation/FluentValidationDefinition.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Jevstafjev.Anecdotes.SubscriberApi.Web.Definitions.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jevstafjev.Anecdotes.SubscriberApi.Web.Definitions.FluentValidation;
+
+public class FluentValidationDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.Configure<ApiBehaviorOptions>(options =>
+        {
+            options.SuppressModelStateInvalidFilter = true;
+        });
+
+        builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+    }
+}

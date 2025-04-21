3017bfd5d2f20016f6abff09ee3610c5706d565d
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-04-21 21:12:12 +0300

    Add UnitOfWork

---
+++ src/Jevstafjev.Anecdotes.SubscriberApi/Jevstafjev.Anecdotes.SubscriberApi.Web/Definitions/UnitOfWork/UnitOfWorkDefinition.cs
@@ -0,0 +1,13 @@
+﻿using Arch.EntityFrameworkCore.UnitOfWork;
+using Jevstafjev.Anecdotes.SubscriberApi.Infrastructure;
+using Jevstafjev.Anecdotes.SubscriberApi.Web.Definitions.Base;
+
+namespace Jevstafjev.Anecdotes.SubscriberApi.Web.Definitions.UnitOfWork;
+
+public class UnitOfWorkDefinition : AppDefinition
+{
+    public override void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddUnitOfWork<ApplicationDbContext>();
+    }
+}

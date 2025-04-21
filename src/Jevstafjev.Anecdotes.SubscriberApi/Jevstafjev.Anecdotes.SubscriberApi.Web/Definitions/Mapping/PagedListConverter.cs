300eb9bb4b2db440d3256593680df65db8d7a3d1
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-04-21 21:14:49 +0300

    Create PagedListConverter.cs

---
+++ src/Jevstafjev.Anecdotes.SubscriberApi/Jevstafjev.Anecdotes.SubscriberApi.Web/Definitions/Mapping/PagedListConverter.cs
@@ -0,0 +1,12 @@
+﻿using Arch.EntityFrameworkCore.UnitOfWork.Collections;
+using AutoMapper;
+
+namespace Jevstafjev.Anecdotes.SubscriberApi.Web.Definitions.Mapping;
+
+public class PagedListConverter<TMapFrom, TMapTo> : ITypeConverter<IPagedList<TMapFrom>, IPagedList<TMapTo>>
+{
+    public IPagedList<TMapTo> Convert(IPagedList<TMapFrom>? source, IPagedList<TMapTo> destination, ResolutionContext context) =>
+        source == null
+            ? PagedList.Empty<TMapTo>()
+            : PagedList.From(source, items => context.Mapper.Map<IEnumerable<TMapTo>>(items));
+}
